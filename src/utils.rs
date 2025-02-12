d485afdbe0f68a9d477abf94a96be99e31357262
allansomensi <allansomensidev@gmail.com>
2025-02-12 12:13:06 -0300

    test(utils): add unit test for `parse_oid_to_vec` function

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use crate::error::AppError;
 
 /// Parses a string representing an OID into a vector of `u64` values.
 ///
-/// This function takes an OID string, splits it by the dot (`.`) separator, and converts each segment into a `u64`
+/// This function takes an OID string.splits it by the dot (`.`) separator, and converts each segment into a `u64`
 /// value. If any segment cannot be parsed into a `u64`, it returns a `SnmpError::InvalidOidFormat` error.
 ///
 /// # Arguments:
@@ -20,4 +20,39 @@ pub fn parse_oid_to_vec(oid: &str) -> Result<Vec<u64>, AppError> {
                 .map_err(|e| AppError::InvalidOidFormat(e.to_string()))
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::parse_oid_to_vec;
+
+    #[test]
+    fn test_parse_oid_to_vec() {
+        let oids = [
+            (
+                "1.3.6.1.2.1.25.3.2.1.3.1",
+                vec![1, 3, 6, 1, 2, 1, 25, 3, 2, 1, 3, 1],
+            ),
+            (
+                "1.3.6.1.2.1.4.21.1.11.169.254.0.0",
+                vec![1, 3, 6, 1, 2, 1, 4, 21, 1, 11, 169, 254, 0, 0],
+            ),
+            (
+                "1.3.6.1.4.1.2699.1.2.1.2.1.1.7.1",
+                vec![1, 3, 6, 1, 4, 1, 2699, 1, 2, 1, 2, 1, 1, 7, 1],
+            ),
+            (
+                "1.3.6.1.2.1.43.11.1.1.9.1.4",
+                vec![1, 3, 6, 1, 2, 1, 43, 11, 1, 1, 9, 1, 4],
+            ),
+            (
+                "1.3.6.1.2.1.4.21.1.13.224.0.0.0",
+                vec![1, 3, 6, 1, 2, 1, 4, 21, 1, 13, 224, 0, 0, 0],
+            ),
+        ];
+
+        for (oid, expected_oid_vec) in oids.iter() {
+            assert_eq!(parse_oid_to_vec(*oid).unwrap(), *expected_oid_vec);
+        }
+    }
 }
