7d71361d211b72a7cd7ba5e23210c56ab0e38592
allansomensi <allansomensidev@proton.me>
2026-02-06 13:31:11 -0300

    feat(snmp): add timeout and retry logic

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -20,43 +20,53 @@ pub struct Args {
     #[arg(long)]
     pub init: bool,
 
-    /// SNMP Protocol Version.
-    #[arg(long, default_value_t = SnmpVersion::V2c, help_heading = "SNMP General")]
+    /// Protocol Version.
+    #[arg(short = 'v', long, default_value_t = SnmpVersion::V2c, help_heading = "SNMP General")]
     pub snmp_version: SnmpVersion,
 
     /// SNMP Service Port.
-    #[arg(long, default_value_t = 161, help_heading = "SNMP General")]
+    #[arg(
+        short = 'p',
+        long,
+        default_value_t = 161,
+        help_heading = "SNMP General"
+    )]
     pub port: u16,
 
-    /// SNMP Community String.
-    #[arg(short, long, default_value = "public", help_heading = "SNMP v1/v2c")]
+    /// Community String.
+    #[arg(
+        short = 'c',
+        long,
+        default_value = "public",
+        help_heading = "SNMP v1/v2c"
+    )]
     pub community: String,
 
-    /// SNMPv3 Username.
+    /// Username.
     #[arg(short = 'u', long, help_heading = "SNMPv3")]
     pub username: Option<String>,
 
-    /// SNMPv3 Security Level.
-    #[arg(short = 'l', long, default_value_t = SecurityLevel::AuthPriv, help_heading = "SNMPv3")]
+    /// Security Level.
+    #[arg(short = 'l', long, default_value_t = SecurityLevel::default(), help_heading = "SNMPv3")]
     pub security_level: SecurityLevel,
 
-    /// SNMPv3 Authentication Protocol.
-    #[arg(short = 'a', long, default_value_t = AuthProtocol::Sha1, help_heading = "SNMPv3")]
+    /// Authentication Protocol.
+    #[arg(short = 'a', long, default_value_t = AuthProtocol::default(), help_heading = "SNMPv3")]
     pub auth_protocol: AuthProtocol,
 
-    /// SNMPv3 Authentication Password.
+    /// Authentication Password.
     #[arg(short = 'A', long, help_heading = "SNMPv3")]
     pub auth_password: Option<String>,
 
-    /// SNMPv3 Privacy Protocol.
-    #[arg(short = 'x', long, default_value_t = PrivacyProtocol::Aes128, help_heading = "SNMPv3")]
+    /// Privacy Protocol.
+    #[arg(short = 'x', long, default_value_t = PrivacyProtocol::default(), help_heading = "SNMPv3")]
     pub privacy_protocol: PrivacyProtocol,
 
-    /// SNMPv3 Privacy Password.
+    /// Privacy Password.
     #[arg(short = 'X', long, help_heading = "SNMPv3")]
     pub privacy_password: Option<String>,
 
-    /// SNMPv3 Context Name.
+    /// Context Name.
     #[arg(short = 'n', long, default_value_t = String::new(), help_heading = "SNMPv3")]
     pub context_name: String,
 
@@ -72,11 +82,19 @@ pub struct Args {
     #[arg(short = 'm', long)]
     pub metrics: bool,
 
+    /// Timeout in seconds.
+    #[arg(short = 't', long, default_value_t = 5)]
+    pub timeout: u64,
+
+    /// Retries.
+    #[arg(short = 'r', long, default_value_t = 3)]
+    pub retries: u8,
+
     /// Select the visual theme for the CLI output.
-    #[arg(long, default_value_t = CliTheme::Solid)]
+    #[arg(long, default_value_t = CliTheme::default())]
     pub theme: CliTheme,
 
     /// Select the output format.
-    #[arg(long, short = 'o', default_value_t = OutputFormat::Text)]
+    #[arg(short = 'o', long, default_value_t = OutputFormat::default())]
     pub output: OutputFormat,
 }
